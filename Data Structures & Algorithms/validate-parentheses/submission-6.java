5eb92164e983edb6b8950836edce1fc98088dcce
vshank-pedd <vrish.pedd@gmail.com>
2026-05-14 17:07:18 -0700

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isOpen (char c){
+        if ((c == '{')||(c == '[')||(c=='(')){
+            return true;
+        }
+        return false;
+    }
+    public boolean isValid(String s) {
+        if ((s.length() == 0) || (s.length() ==1)){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        Map <Character, Character> compliment = new HashMap <>();
+        compliment.put(')','(');
+        compliment.put('}','{');
+        compliment.put(']','[');
+        for (Character c: s.toCharArray()){
+            if (isOpen(c)){
+                stack.push(c);
+                continue;
+            }
+            if ((stack.isEmpty())||(stack.peek() != compliment.get(c))){
+                return false;
+            }
+            if (stack.peek() == compliment.get(c)){
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
