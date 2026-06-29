e1af37bad971f0c3b06773e294a693c94d9c9680
vshank-pedd <vrish.pedd@gmail.com>
2026-06-29 10:12:21 -0700

    Add: sql-sales-person - submission-2

---
+++ SQL/sql-sales-person/submission-2.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+select distinct sales_person.name
+from sales_person
+left join  orders on sales_person.sales_id = orders.sales_id
+where sales_person.sales_id not in (
+    select orders.sales_id
+    from orders
+    where orders.com_id = 1
+)
